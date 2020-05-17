038d7635515c055f80d719ec647e46502eacc5f2
Erawat Chamanont <erawat@gmail.com>
2020-05-17 07:42:31 +0100

    T

--- dist/index.js
+++ dist/index.js
@@ -2361,9 +2361,12 @@ async function run() {
 
 async function applyPatch (id, diffUrl, workingDir) {
   try{
+    console.log(workingDir);
     //pipline does not work
     //await exec.exec(`curl -Ls ${diffUrl} | git apply -v`, null, { cwd: workingDir });
     await exec.exec(`wget ${diffUrl} -O ${id}.diff`, null, { cwd: workingDir });
+    await exec.exec(`pwd`, null, { cwd: workingDir });
+    await exec.exec(`ls -la`, null, { cwd: workingDir });
     await exec.exec(`git apply ${id}.diff`, null, { cwd: workingDir });
     await exec.exec(`rm ${id}.diff`, null, { cwd: workingDir });
   }
