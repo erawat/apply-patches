b822d46ceb0286d35b454626018643a7a16b5ab8
Erawat Chamanont <erawat@gmail.com>
2020-05-17 08:18:18 +0100

    Test

--- dist/index.js
+++ dist/index.js
@@ -2360,14 +2360,8 @@ async function run() {
 }
 
 async function applyPatch (id, diffUrl, workingDir) {
-  try{
-    console.log(workingDir);
-    //pipline does not work
-    //await exec.exec(`curl -Ls ${diffUrl} | git apply -v`, null, { cwd: workingDir });
+  try{ 
     await exec.exec(`curl -Ls ${diffUrl} -o ${id}.diff`, null, { cwd: workingDir });
-    await exec.exec(`pwd`, null, { cwd: workingDir });
-    await exec.exec(`ls -la`, null, { cwd: workingDir });
-    await exec.exec(`cat ${id}.diff`, null, { cwd: workingDir });
     await exec.exec(`git apply ${id}.diff`, null, { cwd: workingDir });
     await exec.exec(`rm ${id}.diff`, null, { cwd: workingDir });
   }
