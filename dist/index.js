53db11546c11527a263bdf6539c4745f7f242aed
Erawat Chamanont <erawat@gmail.com>
2020-05-17 05:37:07 +0100

    fixed workflow

--- dist/index.js
+++ dist/index.js
@@ -2026,7 +2026,7 @@ async function run() {
     const workspace = process.env.GITHUB_WORKSPACE;
     const owner = core.getInput('owner', { required: true });
     const repo = core.getInput('repo', { required: true });
-    const base = core.getInput('base', { required: true });
+    const base = core.getInput('base', { required: false } === 'closed');
 
     const github = new GitHub(process.env.GITHUB_TOKEN);
     const pullRequests = await github.pulls.list({
