30b8a4faa0f9f39e5bcd33326980ba6831c3857f
Erawat Chamanont <erawat@gmail.com>
2020-05-18 14:42:10 +0100

    Test

--- src/apply-patches.js
+++ src/apply-patches.js
@@ -6,58 +6,41 @@ const path = require('path');
 async function run() {
 
   try{
-    const workspace = process.env.GITHUB_WORKSPACE;
-    const workspacePath = path.resolve(workspace);
-
-    const inputToPatchDir =  core.getInput('to_patch_dir', {required: false});
-    const toPatchDir = inputToPatchDir != '' ? workspacePath + path.sep + inputToPatchDir : workspacePath;
 
     const inputOwner = core.getInput('owner', { required: true });
     const inputRepo = core.getInput('repo', { required: true });
     const inputHead = core.getInput('head', { required: true });
     const inputBase = core.getInput('base', { required: true });
 
-   // const inputPattchesBranch = core.getInput('patchesBranch', { required: false });
-
     const github = new GitHub(process.env.GITHUB_TOKEN);
-    const comparedCommits = await github.repos.compareCommits({
+    const comparedCommitsResponse = await github.repos.compareCommits({
       owner: inputOwner,
       repo: inputRepo,
       base: inputBase,
       head: inputHead,
     });
 
-    console.log(comparedCommits);
-  
-    /*const pullRequestsResponse = await github.pulls.list({
-      owner: inputOwner,
-      repo: inputRepo,
-      base: inputBranch,
-      state: state,
+    const data = comparedCommitsResponse.data;
+    if (!Array.isArray(data) || !data.length) {
+     return;
+    }
+   
+    data.forEach(function () {
+      const { id, diff_url: diffUrl } = this;
+      applyPatch(id, diffUrl);
     });
-
-   const pullRequests = pullRequestsResponse.data;
-   if (!Array.isArray(pullRequests) || !pullRequests.length) {
-    throw new Error('No Pull Requests found');
-   }
-
-   pullRequests.forEach(function (data) {
-    const { id, diff_url: diffUrl } = data;
-    console.log(diffUrl);
-    applyPatch(id, diffUrl, toPatchDir);
-   });*/
-
+    
   } 
   catch (error) {
     core.setFailed(error.message);
   }
 }
 
-async function applyPatch (id, diffUrl, workingDir) {
+async function applyPatch (id, diffUrl) {
   try{ 
-    await exec.exec(`curl -Ls ${diffUrl} -o ${id}.diff`, null, { cwd: workingDir });
-    await exec.exec(`git apply -v ${id}.diff `, null, { cwd: workingDir });
-    await exec.exec(`rm ${id}.diff`, null, { cwd: workingDir });
+    await exec.exec(`curl -Ls ${diffUrl} -o ${id}.diff`);
+    await exec.exec(`git apply -v ${id}.diff `);
+    await exec.exec(`rm ${id}.diff`);
   }
   catch (error) {
     core.setFailed(error.message);
