4e2498ba1411f69c5de3545e23cf9383d0140a5e
Erawat Chamanont <erawat@gmail.com>
2020-05-18 16:15:26 +0100

    Test

--- src/apply-patches.js
+++ src/apply-patches.js
@@ -1,12 +1,10 @@
 const core = require('@actions/core');
 const { GitHub } = require('@actions/github');
 const exec = require('@actions/exec');
-const path = require('path')
+const path = require('path');
 
 async function run() {
-
   try{
-
     const inputOwner = core.getInput('owner', { required: true });
     const inputRepo = core.getInput('repo', { required: true });
     const inputHead = core.getInput('head', { required: true });
@@ -19,21 +17,11 @@ async function run() {
       base: inputBase,
       head: inputHead,
     });
-    applyPatch(comparedCommitsResponse.data.diff_url);
+    await applyPatch(comparedCommitsResponse.data.diff_url);
   } 
   catch (error) {
     core.setFailed(error.message);
   }
-}
-
-function getWorkingDir() {
-  const workspace = process.env.GITHUB_WORKSPACE;
-  const workspacePath = path.resolve(workspace);
-
-  const inputPath  =  core.getInput('path', {required: false});
-
-  return workspacePath + path.sep + inputPath;
-
 }
 
 async function applyPatch (diffUrl) {
@@ -42,13 +30,20 @@ async function applyPatch (diffUrl) {
     const workingDir = getWorkingDir();
     const patchFile = 'patch.diff';
     await exec.exec(`curl -Ls ${diffUrl} -o ${patchFile}`, null, { cwd: workingDir });
-    await exec.exec(`pwd`), null, { cwd: workingDir };
     await exec.exec(`patch -p1 -i ${patchFile}`, null, { cwd: workingDir });
     await exec.exec(`rm ${patchFile}`, null, { cwd: workingDir });
   }
   catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function getWorkingDir() {
+  const workspace = process.env.GITHUB_WORKSPACE;
+  const workspacePath = path.resolve(workspace);
+  const inputPath  =  core.getInput('path', {required: false});
+
+  return workspacePath + path.sep + inputPath;
 }
 
 module.exports = run;
